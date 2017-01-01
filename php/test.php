e4967f4d86f42d887772ff1be81823a57e84cdfc
MarioK <mario.kiefer@a1.net>
2017-01-01 01:35:24 +0100

    delete test file

--- php/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo 'hello';
-require_once 'Db.php';
-$db = new Db();
-$rows = $db->select("SELECT lat_user,lon_user FROM user_coords;");
-foreach ($rows as &$row) {
-    $row = array($row['lat_user'], $row['lon_user']);;
-}
-echo (json_encode($rows));

999aa8be0d9dd1b5416dd3a10c4ecdc3c68a8700
MarioK <mario.kiefer@a1.net>
2017-01-15 03:44:22 +0100

    test prepared statement with new mysqli wrapper

--- php/getDelay.php
+++ php/getDelay.php
@@ -1,8 +1,8 @@
 <?php
-require_once 'WetboatDB.php';
+require_once 'MysqliDb.php';
+use Respect\Validation\Validator as v;
 
-$db = new WetboatDB();
+$db2 = new MysqliDb();
+$delay = $db2->getOne("settings", "delay")['delay'];
+echo(json_encode($delay));
 
-$rows = $db->select("SELECT delay FROM settings;") or die(header("HTTP/1.1 500 Kein delay vorhanden"));
-$ret = $rows[0]['delay'];
-echo(json_encode($ret));

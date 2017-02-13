24ba1e415edddcf501013892852af47745b713ba
MarioK <mario.kiefer@a1.net>
2017-02-13 14:51:33 +0100

    update config.php

--- php/config.php
+++ php/config.php
@@ -1,6 +1,8 @@
 <?php
 //Userdata to connect to MySQL server
-$host= "YOUR_HOST";
-$user="YOUR_USERNAME";
-$pwd="YOUR_PASSWORD";
-$db="YOUR_DATABASE";
+$host = "YOUR_HOST";
+$username = "YOUR_USERNAME";
+$password = "YOUR_PASSWORD";
+$db = "YOUR_DATABASE";
+$port = "3306";
+$charset = "utf8";

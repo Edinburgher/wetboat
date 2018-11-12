72189a3f38ff7bb59319c26849fcd8eeada08aec
MarioK <mario.kiefer@a1.net>
2018-11-12 21:40:03 +0100

    add config

---
+++ php/config.php
@@ -0,0 +1,8 @@
+<?php
+//Userdata to connect to MySQL server
+$host = "YOUR_HOST";
+$username = "YOUR_USERNAME";
+$password = "YOUR_PASSWORD";
+$db = "YOUR_DATABASE";
+$port = "3306";
+$charset = "utf8";
